81dfe04253eb40701d6981108e922ba317eec365
Lena Nguyen <lena.nguyen4@gmail.com>
2022-05-11 17:23:01 +0100

    initial commit of kbc example

---
+++ actions/actions.py
@@ -0,0 +1,57 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker, FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+
+class ValidateTestForm(FormValidationAction):
+
+    def name(self):
+        return "validate_test_form"
+
+    def validate_question1(
+            self,
+            value: Text,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        print("hello")
+        return {"question1": value}
+
+
+class ActionSubmitTestForm(Action):
+
+    def name(self) -> Text:
+        return "action_submit_test_form"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Submit test form")
+
+        return []
